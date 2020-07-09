22e4fe418ea9f1325fa53d76ccc2d545287ad4e4
Комиссарова Татьяна <nebykova@office.local>
2020-07-09 15:46:06 +0300

    eslint fixesx

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
             },
           },
         ],
-      }
+      },
     ],
   },
   plugins: [
@@ -52,6 +52,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css',
-    })
+    }),
   ],
 };
